8f6e963e31695e1aa11a092b5624d93fffeaaa75
ashfaq2019 <ashfaqks@yahoo.com>
2020-04-12 23:50:56 -0400

    second2 change

--- Hello.py
+++ Hello.py
@@ -4,11 +4,12 @@ print(sys.version)
 print(sys.executable)
 
 
-def greet(parm1):
-    greeting = "Hello, {}".format(parm1)
+def greet(parm2):
+    greeting = "Hello, {}".format(parm2)
     return greeting
 
 
 print(greet("World"))
 print(greet("Tom"))
-print("This is change testing")
+
+print("This is second change testing")
